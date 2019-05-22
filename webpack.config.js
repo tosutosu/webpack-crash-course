67c86a34baa29493c75f62e8ae8a635e5c9fe4e5
Chida Toshihirro <toshi.c.1986@gmail.com>
2019-05-22 14:10:08 +0900

    eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,12 @@ module.exports = {
     module: {
         rules: [
             {
+                enforce: "pre",
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: "babel-loader",
